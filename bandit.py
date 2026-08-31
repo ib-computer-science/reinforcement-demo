e3ffdc7b526f2f8f93151952391c2463f6ab03c7
Jarmo Hurri <jarmo.hurri@syk.fi>
2026-08-31 10:43:24 +0300

    Use decaying learning rate for true sample-average convergence; bump pulls to 100000

--- bandit.py
+++ bandit.py
@@ -1,10 +1,10 @@
 import random
 
 epsilon = 0.1              # probability of exploring instead of exploiting
-learning_rate = 0.1        # how fast Q-estimates move toward observed reward
-num_steps = 1000
+num_steps = 100000
 
 Q = [0.0, 0.0]              # estimated value of each arm, starts at zero
+times_chosen = [0, 0]       # how many times each arm has been pulled so far
 true_p = [0.3, 0.7]         # true (hidden) probability of reward for each arm; arm 1 is better
 
 for step in range(num_steps):
@@ -20,6 +20,8 @@ for step in range(num_steps):
     reward = 1 if win_roll < true_p[chosen_arm] else 0
 
     # --- update our value estimate for the chosen arm toward the observed reward ---
+    times_chosen[chosen_arm] += 1
+    learning_rate = 1 / times_chosen[chosen_arm]   # decays as an arm is pulled more, giving a true running average
     prediction_error = reward - Q[chosen_arm]
     Q[chosen_arm] += learning_rate * prediction_error
 
